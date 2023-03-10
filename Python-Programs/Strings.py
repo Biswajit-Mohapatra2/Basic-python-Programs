dc0b40f05e9b07e0353c32e2dfb2f8fde95fc3b0
BiswajitGit <biswajitmohapatra2255@gmail.com>
2023-03-10 15:52:20 +0530

    String operators

---
+++ Python-Programs/Strings.py
@@ -0,0 +1,10 @@
+phase = "Biswajit Mohapatra"
+print(phase.lower())
+print(phase.upper())
+print(phase.islower())
+print(phase.isupper())
+
+print(len(phase))
+print(phase[0])
+print(phase.index('jit'))
+print(phase.replace("Biswajit", "Satyajit"))
