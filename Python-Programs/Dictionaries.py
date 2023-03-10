1a3cc7d6affe9293e231eead7b59cb96953a362d
BiswajitGit <biswajitmohapatra2255@gmail.com>
2023-03-10 15:49:59 +0530

    Dictionary Profram

---
+++ Python-Programs/Dictionaries.py
@@ -0,0 +1,17 @@
+monthconversion = {
+    0 : "January",
+    1 : "February",
+    2 : "March",
+    3 : "April",
+    4 : "May",
+    5 : "june",
+    6 : "July",
+    7 : "August",
+    8 : "Sepetember",
+    9 : "octobor",
+    10 : "November",
+    11 : "December",
+
+}
+print(monthconversion[6])
+print(monthconversion.get(12, "Not Available"))
