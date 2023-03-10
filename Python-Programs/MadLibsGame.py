6126ee6103ef0b8bd8e2f48e0229bc6a1c3205c8
BiswajitGit <biswajitmohapatra2255@gmail.com>
2023-03-10 15:53:03 +0530

    madlips game

---
+++ Python-Programs/MadLibsGame.py
@@ -0,0 +1,7 @@
+color = input("Enter Your Color Name: ")
+noun = input("Enter Your Noun: ")
+celebrity = input("Enter Your Fav Celebrity Name: ")
+
+print("Roses are " + color)
+print(noun + " are blue")
+print("i love to watch " + celebrity)
