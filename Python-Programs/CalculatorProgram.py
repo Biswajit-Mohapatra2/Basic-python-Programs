5b6487ea50beaff343cd011a17cae604ed24f6b8
BiswajitGit <biswajitmohapatra2255@gmail.com>
2023-03-10 14:54:03 +0530

    Change Added2

---
+++ Python-Programs/CalculatorProgram.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter your First Number:"))
+Operator = input("Enter your Operator: ")
+num2 = float(input("Enter Your Second Number: "))
+
+if Operator == "+":
+    print("The answer is: "+ str(num1 + num2))
+elif Operator == "-":
+    print("The answer is: "+ str(num1 - num2))
+elif Operator == "*":
+    print("The answer is: "+ str(num1 * num2))
+elif Operator == "/":
+    print("The answer is: "+ str(num1 / num2))
+else:
+    print("Invalid Number You Entered..")
