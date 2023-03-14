8c2ce13cd71ee5d5bb8d12801add8e7c980ee945
BiswajitGit <biswajitmohapatra2255@gmail.com>
2023-03-14 20:14:47 +0530

    printsumofnumbertill10

---
+++ Python-Programs/printnumbertill100.py
@@ -0,0 +1,12 @@
+
+total = 0
+
+number = int(input('Enter a number: '))
+
+while number != 10:
+    total += number    # total = total + number
+    
+    number = int(input('Enter a number: '))
+    
+
+print('total =', total)
